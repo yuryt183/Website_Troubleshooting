95ff374dabc5d6f7c494f0038ace5bba3b2ee530
yuryt183 <119700465+yuryt183@users.noreply.github.com>
2024-01-30 22:41:35 -0500

    Add files via upload

---
+++ assets/js/sketch.js
@@ -0,0 +1,46 @@
+let balls = [];
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    for (let i = 0; i < 20; i++) {
+        balls.push(new Ball());
+    }
+}
+
+function draw() {
+    background(255, 255, 255, 25); // Semi-transparent background (creates the trail effect)
+    balls.forEach(ball => {
+        ball.move();
+        ball.display();
+    });
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
+
+class Ball {
+    constructor() {
+        this.x = random(width);
+        this.y = random(height);
+        this.diameter = random(10, 50);
+        this.xSpeed = random(-2, 2);
+        this.ySpeed = random(-2, 2);
+        this.color = [random(255), random(255), random(255), random(50, 150)]; // RGBA
+    }
+
+    move() {
+        this.x += this.xSpeed;
+        this.y += this.ySpeed;
+
+        // Bounce off edges
+        if (this.x < 0 || this.x > width) this.xSpeed *= -1;
+        if (this.y < 0 || this.y > height) this.ySpeed *= -1;
+    }
+
+    display() {
+        noStroke();
+        fill(this.color);
+        ellipse(this.x, this.y, this.diameter, this.diameter);
+    }
+}
