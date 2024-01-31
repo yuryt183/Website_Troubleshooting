7c615ec7505ac271660b95bed53231876f92fd04
yuryt183 <119700465+yuryt183@users.noreply.github.com>
2024-01-31 16:54:26 -0500

    Update sketch.js

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -1,46 +1,41 @@
-let balls = [];
-let bgImage; // Variable to store background image
-
-function preload() {
-  // Assuming the image is in the same directory as your sketch
-  bgImage = loadImage('assets/img/background.jpeg'); 
-}
-
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  // Create 20 balls
-  for (let i = 0; i < 20; i++) {
-    balls.push(new Ball());
-  }
-}
-
-function draw() {
-  // Draw the background image at position (0, 0) and resize it to the window's width and height
-  image(bgImage, 0, 0, windowWidth, windowHeight);
-
-  // Update and display balls
-  balls.forEach(ball => {
-    ball.update(mouseX, mouseY); // Pass the mouse position to each ball
-    ball.display();
-  });
-}
-
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-}
-
 class Ball {
   constructor() {
     this.position = createVector(random(width), random(height));
+    this.velocity = p5.Vector.random2D().mult(random(1, 3)); // Random velocity
     this.diameter = random(10, 50);
-    this.color = [random(255), random(255), random(255), random(50, 150)]; // RGBA
+    this.color = [random(255), random(255), random(255), random(50, 150)];
   }
 
-  // Update ball's position to follow the cursor
-  update(targetX, targetY) {
-    // Move towards the cursor using p5.Vector lerp
-    let target = createVector(targetX, targetY);
-    this.position.lerp(target, 0.05); // 0.05 is the amount of easing
+  update() {
+    // Update position based on velocity
+    this.position.add(this.velocity);
+
+    // Check for collision with walls and reverse velocity if necessary
+    if (this.position.x < 0 || this.position.x > width) {
+      this.velocity.x *= -1;
+    }
+    if (this.position.y < 0 || this.position.y > height) {
+      this.velocity.y *= -1;
+    }
+
+    // Check for collision with other balls
+    for (let other of balls) {
+      if (other !== this && this.isColliding(other)) {
+        this.handleCollision(other);
+      }
+    }
+  }
+
+  isColliding(other) {
+    let distance = p5.Vector.dist(this.position, other.position);
+    return distance < (this.diameter / 2 + other.diameter / 2);
+  }
+
+  handleCollision(other) {
+    // Simple collision response
+    let temp = this.velocity.copy();
+    this.velocity = other.velocity.copy();
+    other.velocity = temp;
   }
 
   display() {
@@ -49,3 +44,5 @@ class Ball {
     ellipse(this.position.x, this.position.y, this.diameter, this.diameter);
   }
 }
+
+// Rest of your setup and draw functions remain the same
